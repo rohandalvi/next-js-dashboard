b8e4770c7f916af712a5fc0bd786046624c6450c
rohan <rohan_dalvi@icloud.com>
2023-12-29 22:00:23 -0800

    fix path name

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -5,7 +5,7 @@ import {
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import {useParams, usePathname, userPathName} from 'next/navigation';
+import {useParams, usePathname} from 'next/navigation';
 import clsx from 'clsx';
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
